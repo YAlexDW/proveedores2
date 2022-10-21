b4298d0f157f2ad4ca1abe6e975c9bbd405fdb35
mnnq <milly.nunez.mt@correo.usa.edu.co>
2022-10-21 16:34:52 -0500

    diseño

--- frontend/src/app/auth/gcg/gcg.component.ts
+++ frontend/src/app/auth/gcg/gcg.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { IcarouselItem } from './Icarousel-item.metadata';
 
 @Component({
   selector: 'app-gcg',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gcg.component.css']
 })
 export class GCGComponent implements OnInit {
+  @Input() height = 500;
+  @Input() isFullScreen = false;
+  @Input() items: IcarouselItem[]=[];
 
-  constructor() { }
+  public finalHeight: String|number = 0;
+  public currentPosition = 0;
+
+  constructor() {
+    this.finalHeight = this.isFullScreen ? '100vh': `${this.height}px`
+  }
 
   ngOnInit(): void {
+    this.items.map((i,index)=>{
+      i.id = index;
+      i.marginLeft = 0;
+    });
+  }
+
+  setCurrentPOsition(position : number){
+    this.currentPosition = position;
   }
 
 }
